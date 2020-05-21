c1b4f1e584166b0b12c2971a958868d7ff447934
Luigi Maiorano <lmaiorano97@gmail.com>
2020-05-21 13:29:38 +0200

    Basic Package files

---
+++ LuigiPyTools/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+title: __init__
+project: LuigiPyTools
+date: 5/21/2020
+author: lmaio
+"""
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+title: setup
+project: LuigiPyTools
+date: 5/21/2020
+author: lmaio
+"""
+
+from setuptools import setup
+
+setup(
+    name='LuigiPyTools',
+    packages=['LuigiPyTools'],
+    description='Tools commonly used in engineering projects',
+    version='0.1',
+    url='https://github.com/LMaiorano/LuigiPyTools',
+    author='LMaiorano',
+    author_email='lmaiorano97@gmail.com',
+    keywords=['pip', 'lmaiorano', 'pytools']
+)
